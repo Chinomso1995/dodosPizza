00962de5b0115bca3cad8dac00950a9a5f065054
chinomso1995 <chinomsojohnson@gmail.com>
2020-07-09 22:15:37 +0100

    Updates

---
+++ precache-manifest.cd8b1aab8fc69aedd0ee1e2a6a5e1563.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3bf92e72a38bed199b90cb677ba282",
+    "url": "/dodosPizza/index.html"
+  },
+  {
+    "revision": "ca643a45d4dff969ac00",
+    "url": "/dodosPizza/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "17ebd8fa1fb2f26d73c1",
+    "url": "/dodosPizza/static/css/main.3a1ac2d0.chunk.css"
+  },
+  {
+    "revision": "ca643a45d4dff969ac00",
+    "url": "/dodosPizza/static/js/2.3894e8aa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dodosPizza/static/js/2.3894e8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ebd8fa1fb2f26d73c1",
+    "url": "/dodosPizza/static/js/main.7c17252c.chunk.js"
+  },
+  {
+    "revision": "503db060318336047b49",
+    "url": "/dodosPizza/static/js/runtime-main.aeac290a.js"
+  },
+  {
+    "revision": "8449c5ebb61d16d5c223430e1df4ba15",
+    "url": "/dodosPizza/static/media/beefsuya.8449c5eb.jpg"
+  },
+  {
+    "revision": "747bc48e7b77e51b237c862c74305712",
+    "url": "/dodosPizza/static/media/cheeseburgerpizza.747bc48e.jpg"
+  },
+  {
+    "revision": "9adea3092d8256f5252a7a02ad8f0d0c",
+    "url": "/dodosPizza/static/media/cheesychicken.9adea309.jpg"
+  },
+  {
+    "revision": "71ce59ffa530a05ccaf487121380117a",
+    "url": "/dodosPizza/static/media/chickenbbq.71ce59ff.jpg"
+  },
+  {
+    "revision": "0b96387ad7c3ff026d0b382964eeb03e",
+    "url": "/dodosPizza/static/media/chickencurry.0b96387a.jpeg"
+  },
+  {
+    "revision": "946f228b3fbb8d10522f23e1dc6edcac",
+    "url": "/dodosPizza/static/media/chickensupreme.946f228b.jpg"
+  },
+  {
+    "revision": "786ec39091e837a49eb26d7e1916fcaa",
+    "url": "/dodosPizza/static/media/chickensuya.786ec390.jpg"
+  },
+  {
+    "revision": "0d1c7fc3f885705f8f9ea457391ee980",
+    "url": "/dodosPizza/static/media/fifth-carousel-image.0d1c7fc3.jpeg"
+  },
+  {
+    "revision": "82dd41073874d5dbebd93690681a6bda",
+    "url": "/dodosPizza/static/media/first-carousel-image.82dd4107.jpeg"
+  },
+  {
+    "revision": "938cd4f4aaadd9ed9c99b0613be4e0ec",
+    "url": "/dodosPizza/static/media/fourth-carousel-image.938cd4f4.jpeg"
+  },
+  {
+    "revision": "d75f416fca7ff274d9559b7d44930d60",
+    "url": "/dodosPizza/static/media/halfpizza.d75f416f.jpg"
+  },
+  {
+    "revision": "67b3505dff7308699f6f84bd4af00c39",
+    "url": "/dodosPizza/static/media/hawaiin.67b3505d.jpg"
+  },
+  {
+    "revision": "f64bb99147cd801705336cb9a79e446a",
+    "url": "/dodosPizza/static/media/logo.f64bb991.png"
+  },
+  {
+    "revision": "c66f9a11d3d3ed50a74f1d28bf1019e9",
+    "url": "/dodosPizza/static/media/margherita.c66f9a11.jpg"
+  },
+  {
+    "revision": "fb0ab3b8c96f8cf1da027c30a3bef659",
+    "url": "/dodosPizza/static/media/meatybbq.fb0ab3b8.jpg"
+  },
+  {
+    "revision": "ed2ceb729cf83b68c201ea9ba83e2372",
+    "url": "/dodosPizza/static/media/meatyoverload.ed2ceb72.jpg"
+  },
+  {
+    "revision": "6b874d35bd267bf632d1aac6e7c4700e",
+    "url": "/dodosPizza/static/media/pepperoni.6b874d35.jpg"
+  },
+  {
+    "revision": "144306c8ede827f9e9c83e493a5c4cfa",
+    "url": "/dodosPizza/static/media/pepperonifresh.144306c8.jpeg"
+  },
+  {
+    "revision": "1a2936dba8e45a34a63191ef43d15e7a",
+    "url": "/dodosPizza/static/media/second-carousel-image.1a2936db.jpeg"
+  },
+  {
+    "revision": "7daa15e1a27749c321e80449b28eb4e3",
+    "url": "/dodosPizza/static/media/sharwarmapizza.7daa15e1.jpg"
+  },
+  {
+    "revision": "5d805fba0469d160561f85164eca7f06",
+    "url": "/dodosPizza/static/media/spicymixedpizza.5d805fba.jpg"
+  },
+  {
+    "revision": "2522eb5484013125d07ead620bf0caf7",
+    "url": "/dodosPizza/static/media/supermeaty.2522eb54.jpg"
+  },
+  {
+    "revision": "b1afd997f472b42ea4af7a127db5e34f",
+    "url": "/dodosPizza/static/media/sweetchillichicken.b1afd997.jpg"
+  },
+  {
+    "revision": "a55f2210fae0cfe4e58c6fc0071c41dc",
+    "url": "/dodosPizza/static/media/third-carousel-image.a55f2210.jpeg"
+  },
+  {
+    "revision": "f327c1db72a4a4a5c1907ea04af1d72c",
+    "url": "/dodosPizza/static/media/veggieoverload.f327c1db.jpg"
+  }
+]);
